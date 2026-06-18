831ea2c4308c828baa5e435d241a68439fdfb814
Mohit Kumar <your-github-email@gmail.com>
2026-06-18 17:48:01 +0530

    Create a method for user_Creating just for testing purpost

---
+++ src/main/java/com/app/Ecom_application/Enities/User.java
@@ -0,0 +1,19 @@
+package com.app.Ecom_application.Enities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class User 
+{
+ private Long id ;
+ private String firstName ;
+ private String lastName ;
+}
--- src/main/java/com/app/Ecom_application/controller/UserController.java
+++ src/main/java/com/app/Ecom_application/controller/UserController.java
@@ -1,5 +1,51 @@
 package com.app.Ecom_application.controller;
 
-public class UserController {
+import org.springframework.web.bind.annotation.RestController;
 
+import com.app.Ecom_application.Enities.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+public class UserController 
+{
+   List<User> userList = new ArrayList<>();
+
+    @GetMapping("test")
+    public String testMethod() {
+        return new String("This is testing method of UserController");
+    }
+    
+    @GetMapping("ecom-users")
+    public List<User> getUser()
+    {
+       
+        return  userList;
+    }
+    // @PostMapping("path")
+    // public ResponseEntity CreateUser (@RequestBody String entity) {
+        
+    //   User user=new User();
+    //   user.setFirst("Mohit");
+    //   user.setLast("Kabir");
+    //     return ResponseEntity.status(HttpStatus.OK).body(user);
+    // }
+    @PostMapping("create/user")
+    public List<User> CreateUser (@RequestBody User user) {
+        System.out.println("User is :"+user);
+         userList.add(user);
+         return userList;
+      
+    }
 }
